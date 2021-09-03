1528ba14c6623713c2f094198c6074369365157f
Mario <mpel@protonmail.com>
2021-09-03 10:37:10 +0200

    Adding HLT

--- generation/testScript_queue.sh
+++ generation/testScript_queue.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-HOMEDIR=/afs/cern.ch/user/p/pellicci/work/ZThreeGamma/Production/CMSSW_10_6_17_patch1/src/StandardModel/ZThreeGamma/generation
-CMSSW_TO_USE=CMSSW_10_6_17
-INPUTDIR=/eos/user/p/pellicci/ZThreeGamma_root/2016/SIM
-OUTPUTDIR=/eos/user/p/pellicci/ZThreeGamma_root/2016/DIGI
-PYTHONAME=ZToThreeGamma_DIGI_2016_cfg.py
+HOMEDIR=/afs/cern.ch/user/p/pellicci/work/ZThreeGamma/Production/CMSSW_8_0_33_UL/src/StandardModel/ZThreeGamma/generation
+CMSSW_TO_USE=CMSSW_8_0_33_UL
+INPUTDIR=/eos/user/p/pellicci/ZThreeGamma_root/2016/DIGI
+OUTPUTDIR=/eos/user/p/pellicci/ZThreeGamma_root/2016/HLT
+PYTHONAME=ZToThreeGamma_HLT_2016_cfg.py
+
+NEVENTS=$1
+FILENAME=$2
 
 #this is necessary only if EOS access is required
 export X509_USER_PROXY=/afs/cern.ch/user/p/pellicci/voms_proxy/x509up_u28550
 
-voms-proxy-info -all
-voms-proxy-info -all -file $X509_USER_PROXY
+#voms-proxy-info -all
+#voms-proxy-info -all -file $X509_USER_PROXY
 
 if [ "$1" == "" ]; then
  echo "Specify number of events and base output file, eg: source testScript_queue.sh 50 test"
@@ -25,13 +28,10 @@ if [ "$4" == "" ]; then
  OFFSET=0
 fi
 
-NEVENTS=$1
-OUTPUT=$2
-
 export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
 source $VO_CMS_SW_DIR/cmsset_default.sh
 
-export SCRAM_ARCH=slc7_amd64_gcc820
+#export SCRAM_ARCH=slc7_amd64_gcc820
 if [ -r $CMSSW_TO_USE/src ] ; then 
  echo release $CMSSW_TO_USE already exists
 else
@@ -53,8 +53,8 @@ EOF
 
 jobNumber=$(($3+$OFFSET));
 
-xrdcp $INPUTDIR/ZThreeGamma_${jobNumber}.root .
+xrdcp $INPUTDIR/${FILENAME}_${jobNumber}.root .
 
-cmsRun $PYTHONAME $NEVENTS ZThreeGamma_${jobNumber}.root
+cmsRun $PYTHONAME $NEVENTS ${FILENAME}_${jobNumber}.root
 
-xrdcp process.root $OUTPUTDIR/${OUTPUT}_${jobNumber}.root
+xrdcp process.root $OUTPUTDIR/${FILENAME}_${jobNumber}.root
